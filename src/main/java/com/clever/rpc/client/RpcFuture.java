54c20784f2d0cd3f2de0b3cc5679b62114bfac33
sunbin <sunbin@sudiyi.cn>
2019-05-07 16:54:43 +0800

    优化，去掉多余的getState();

--- src/main/java/com/clever/rpc/client/RpcFuture.java
+++ src/main/java/com/clever/rpc/client/RpcFuture.java
@@ -116,7 +116,6 @@ public class RpcFuture implements Future<Object> {
         }
 
         public boolean isDone() {
-            getState();
             return getState() == done;
         }
     }
