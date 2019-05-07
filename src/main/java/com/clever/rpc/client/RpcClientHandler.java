cb668b8c37b85c353124d887f2e3264fa2b0c5a6
sunbin <sunbin@sudiyi.cn>
2019-05-07 18:30:54 +0800

    优化，增加注释

--- src/main/java/com/clever/rpc/client/RpcClientHandler.java
+++ src/main/java/com/clever/rpc/client/RpcClientHandler.java
@@ -7,6 +7,7 @@ import io.netty.channel.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.net.SocketAddress;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -14,21 +15,45 @@ import java.util.concurrent.ConcurrentHashMap;
  * @author sunbin
  */
 public class RpcClientHandler extends SimpleChannelInboundHandler<RpcResponse> {
+
     private static final Logger logger = LoggerFactory.getLogger(RpcClientHandler.class);
 
-    private ConcurrentHashMap<String, RpcFuture> pendingRPC = new ConcurrentHashMap<String, RpcFuture>();
+    private ConcurrentHashMap<String, RpcFuture> pendingRPC = new ConcurrentHashMap<>();
 
     private volatile Channel channel;
+
+    private SocketAddress remoteAddress;
+
+    public SocketAddress getRemoteAddress() {
+        return remoteAddress;
+    }
+
 
     public Channel getChannel() {
         return channel;
     }
 
+    /**
+     * <p>
+     *     绑定ip和端口后触发
+     * </p>
+     * @param ctx
+     * @throws Exception
+     */
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        this.remoteAddress = this.channel.remoteAddress();
         super.channelActive(ctx);
     }
 
+    /**
+     * <p>
+     *     在ServerSocketChannel对应的pipeline中触发（channelFuture.addListener触发）
+     * </p>
+     *
+     * @param ctx
+     * @throws Exception
+     */
     @Override
     public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
         super.channelRegistered(ctx);
@@ -59,19 +84,19 @@ public class RpcClientHandler extends SimpleChannelInboundHandler<RpcResponse> {
     }
 
     public RpcFuture sendRequest(RpcRequest request) {
-
         CompletableFuture<ChannelFuture> completableFuture = new CompletableFuture<>();
         RpcFuture rpcFuture = new RpcFuture(request);
         pendingRPC.put(request.getRequestId(), rpcFuture);
-        channel.writeAndFlush(request).addListener((ChannelFutureListener) future -> completableFuture.complete(future));
-
-        //取代 CountDownLatch 的 await
-        CompletableFuture.allOf(completableFuture);
+        channel.writeAndFlush(request).addListener((ChannelFutureListener) future -> {
+            completableFuture.complete(future);
+            //取代 CountDownLatch 的 await
+            CompletableFuture.allOf(completableFuture);
 //        try {
 //            latch.await();
 //        } catch (InterruptedException e) {
 //            logger.error(e.getMessage());
 //        }
+        });
 
         return rpcFuture;
     }
