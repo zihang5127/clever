ebf09386f82593b20eab057c0644205af066e7c5
sunbin <sunbin@sudiyi.cn>
2019-05-08 17:00:20 +0800

    优化，增加注释

--- src/main/java/com/clever/rpc/client/ConnectManage.java
+++ src/main/java/com/clever/rpc/client/ConnectManage.java
@@ -30,6 +30,15 @@ public class ConnectManage {
     private static ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(16, 16,
             600L, TimeUnit.SECONDS, new ArrayBlockingQueue<>(65536));
     private volatile boolean isRuning = true;
+
+    /**
+     * <p>
+     *      CopyOnWrite容器即写时复制的容器。通俗的理解是当我们往一个容器添加元素的时候，不直接往当前容器添加，而是
+     *      先将当前容器进行Copy，复制出一个新的容器，然后新的容器里添加元素，添加完元素之后，再将原容器的引用指向
+     *      新的容器。这样做的好处是我们可以对CopyOnWrite容器进行并发的读，而不需要加锁，因为当前容器不会添加任何
+     *      元素。所以CopyOnWrite容器也是一种读写分离的思想，读和写不同的容器
+     * </p>
+     */
     private CopyOnWriteArrayList<RpcClientHandler> connectedHandlers = new CopyOnWriteArrayList<>();
     private Map<InetSocketAddress, RpcClientHandler> connectedServerNodes = new ConcurrentHashMap<>();
     private ReentrantLock lock = new ReentrantLock();
